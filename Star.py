bcacb3fc056105d86c9d5aa0922b49a3451af6dd
Ario-sut <rakaario6@gmail.com>
2023-03-31 17:28:39 +0700

    doraemon turtle

--- Star.py
+++ Star.py
@@ -7,8 +7,8 @@ t.speed(1)
 
 def drawn(n, x, angle):
 
-    t.colormode(255)
-
+    #t.colormode(255)
+    
     a = random.randint(0, 255)
     b = random.randint(0, 255)
     c = random.randint(0, 255)
@@ -16,7 +16,7 @@ def drawn(n, x, angle):
     t.pencolor(a,b,c)
     t.fillcolor(a,b,c)
 
-    t.begin_fill()
+    t.begin_fill(),
 
     for j in range(5):
         t.forward(5*n-5*j)
@@ -26,7 +26,7 @@ def drawn(n, x, angle):
 
     t.end_fill()
 
-    t.rt(angle)
+    t.rt(angle) 
 
 n=30
 x=144
